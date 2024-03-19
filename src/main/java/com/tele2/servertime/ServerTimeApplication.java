73a7289e1e63464648dd002ea5ead884079c56a7
trhz <treezmusic@yandex.ru>
2024-03-19 18:43:38 +0300

    Сборка проекта завершена

---
+++ src/main/java/com/tele2/servertime/ServerTimeApplication.java
@@ -0,0 +1,13 @@
+package com.tele2.servertime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tele2/servertime/ServerTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tele2.servertime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
